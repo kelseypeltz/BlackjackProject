c39e1600ee5681de5775ee1a335f93e8f6000f06
kelseypeltz <69976409+kelseypeltz@users.noreply.github.com>
2023-03-28 01:38:53 -0500

    Add files via upload
    worked on website formatting

--- Milestone 6/main.py
+++ Milestone 6/main.py
@@ -1,59 +1,80 @@
-﻿from flask import Flask
+from flask import Flask
 from flask import request
 from flask import render_template
 
 import bottraining as bt
 
+
 app = Flask(__name__)
+
 
 @app.route("/")
 def index():
+    title = "Blackjack Game Settings"
+    description = "This is Jake and Kelsey's capstone project. Configure the settings for your blackjack game below."
+
     return (
-        """<form method="POST" action="/submit">
-            <label for="num_decks">Enter number of decks:</label>
-            <input type="number" id="num_decks" name="num_decks" min="1">
-
-            <label for="typical_settings">Do you want to use typical amount of initial dealt cards (2 cards per player) and blackjack winning points (sum of 21)?</label>
-            <select id="typical_settings" name="typical_settings">
-              <option value="yes">Yes</option>
-              <option value="no">No</option>
-            </select>
-
-            <div id="custom_settings" style="display:none;">
-              <label for="initial_dealt_cards">Enter the number of initial dealt cards:</label>
-              <input type="number" id="initial_dealt_cards" name="initial_dealt_cards" min="1" value="2"><br>
-
-              <label for="winning_points">Enter the number of winning points for blackjack:</label>
-              <input type="number" id="winning_points" name="winning_points" min="1" value="21">
-            </div>
-
-            <label for="hit_or_stand">Select the dealer's strategy:</label>
-            <select id="hit_or_stand" name="hit_or_stand">
-              <option value="18">Dealer hits on soft 17</option>
-              <option value="17">Dealer stands on soft 17</option>
-            </select>
-
-            <label for="double_cards">What cards are you permitted to double?</label>
-            <select id="double_cards" name="double_cards">
-              <option value="1">Any 2 cards</option>
-              <option value="2">9, 10 &amp; 11 only</option>
-              <option value="3">10 &amp; 11 only</option>
-            </select>
-
-            <button type="submit">Submit</button>
-          </form>
-
-
-            <script>
-              // Show/hide custom settings based on user input
-              document.querySelector('#typical_settings').addEventListener('change', function() {
-                if (this.value === 'no') {
-                  document.querySelector('#custom_settings').style.display = 'block';
-                } else {
-                  document.querySelector('#custom_settings').style.display = 'none';
-                }
-              });
-            </script>"""
+        """<html>
+                <head>
+                    <title>Blackjack Game Settings</title>
+                    <style>
+                        img {
+                            width: 50%;
+                            height: auto;
+                        }
+                    </style>
+                </head>
+                <body>
+                    <img src="images/first_image.jpg" alt="First Image">
+                    <h1>Blackjack Game Settings</h1>
+                    <p>This is Jake and Kelsey's capstone project. Configure the settings for your blackjack game below.</p>
+                    <form method="POST" action="/submit">
+                        <label for="num_decks">Enter number of decks:</label>
+                        <input type="number" id="num_decks" name="num_decks" min="1">
+            
+                        <label for="typical_settings">Do you want to use typical amount of initial dealt cards (2 cards per player) and blackjack winning points (sum of 21)?</label>
+                        <select id="typical_settings" name="typical_settings">
+                          <option value="yes">Yes</option>
+                          <option value="no">No</option>
+                        </select>
+            
+                        <div id="custom_settings" style="display:none;">
+                          <label for="initial_dealt_cards">Enter the number of initial dealt cards:</label>
+                          <input type="number" id="initial_dealt_cards" name="initial_dealt_cards" min="1" value="2"><br>
+            
+                          <label for="winning_points">Enter the number of winning points for blackjack:</label>
+                          <input type="number" id="winning_points" name="winning_points" min="1" value="21">
+                        </div>
+            
+                        <label for="hit_or_stand">Select the dealer's strategy:</label>
+                        <select id="hit_or_stand" name="hit_or_stand">
+                          <option value="18">Dealer hits on soft 17</option>
+                          <option value="17">Dealer stands on soft 17</option>
+                        </select>
+            
+                        <label for="double_cards">What cards are you permitted to double?</label>
+                        <select id="double_cards" name="double_cards">
+                          <option value="1">Any 2 cards</option>
+                          <option value="2">9, 10 &amp; 11 only</option>
+                          <option value="3">10 &amp; 11 only</option>
+                        </select>
+            
+                        <button type="submit">Submit</button>
+                      </form>
+            
+            
+                        <script>
+                          // Show/hide custom settings based on user input
+                          document.querySelector('#typical_settings').addEventListener('change', function() {
+                            if (this.value === 'no') {
+                              document.querySelector('#custom_settings').style.display = 'block';
+                            } else {
+                              document.querySelector('#custom_settings').style.display = 'none';
+                            }
+                        });
+            </script>
+        </body>
+    </html>"""
     )
 
 @app.route("/submit", methods=["POST"])
@@ -99,193 +120,136 @@ def submit():
 
 
     # report strategy result
+    result = "" 
+    
+    # add header and statistics for Q-learning
+    result += "<h3>Q-learning</h3>"
+    
+# create Q-learning strategy chart
+    result += "<table>"
 
-    result = ""
-    for i in range(0, winningPoints*3-17):
-        result += " "
-    result +="Q-learning"
-    result += "<br>"
+    # create header row
+    result += "<tr><td rowspan='12'>Dealer's Upcard</td><td colspan='12'>player (usable ace)</td><td></td><td colspan='12'>player (no usable ace)</td></tr>"
 
-    result +="              player (usable ace)"
-    for i in range(0, winningPoints*3-36):
-        result +=" "
-    result +="player (no usable ace)<br>"
+# create top row of player cards
 
-    result +="         "
+    # create top row of player cards
+    result += "<tr><td></td>"
     for p in range(11, winningPoints+1):
-        result += str(p)
-    result +="             "
+        result += "<td>{}</td>".format(p)
+    result += "<td></td>"
     for p in range(11, winningPoints+1):
-        result += str(p)
-    result += "<br>"
-
-    for d in range (1,11):
-        if (d == 10):
-            result +="      10   "
-        elif (d == 3):
-            result +="    D  3   "
-        elif (d == 4):
-            result +="    e  4   "
-        elif (d == 5):
-            result +="    a  5   "
-        elif (d == 6):
-            result +="    l  6   "
-        elif (d == 7):
-            result +="    e  7   "
-        elif (d == 8):
-            result +="    r  8   "
-        else:
-            result += "      " + str(d) + "  "
+        result += "<td>{}</td>".format(p)
+    result += "</tr>"
+    
+    # create rows for each dealer card
+    for d in range(1, 11):
+        # create row for dealer card
+        result += "<tr>"
+        # label dealer card
+        result += "<td>{}</td>".format(d if d != 1 else "A")
+        # create cells for player (usable ace)
         for p in range(11, winningPoints+1):
-            result +=HitStickOrDouble(QTableDictForQL[p,d,1,0], QTableDictForQL[p,d,1,1], QTableDictForQL[p,d,1,2])
-
-        if (d == 10):
-            result +="         10  "
-        elif (d == 3):
-            result +="      D   3  "
-        elif (d == 4):
-            result +="      e   4  "
-        elif (d == 5):
-            result +="      a   5  "
-        elif (d == 6):
-            result +="      l   6  "
-        elif (d == 7):
-            result +="      e   7  "
-        elif (d == 8):
-            result +="      r   8  "
-        else:
-            result += "      " + str(d) + "  "
-
+            cell_value = HitStickOrDouble(QTableDictForQL[p,d,1,0], QTableDictForQL[p,d,1,1], QTableDictForQL[p,d,1,2])
+            result += "<td>{}</td>".format(cell_value)
+        # label dealer card
+        result += "<td>{}</td>".format(d if d != 1 else "A")
+        # create cells for player (no usable ace)
         for p in range(11, winningPoints+1):
-            result +=HitStickOrDouble(QTableDictForQL[p,d,0,0], QTableDictForQL[p,d,0,1], QTableDictForQL[p,d,0,2])
+            cell_value = HitStickOrDouble(QTableDictForQL[p,d,0,0], QTableDictForQL[p,d,0,1], QTableDictForQL[p,d,0,2])
+            result += "<td>{}</td>".format(cell_value)
+        # close row
+        result += "</tr>"    
+    # close table
+    result += "</table>"    
+    
+        # add header and statistics for Sarsa
+    result += "<h3>Sarsa</h3>"
 
-        result += "<br>"
-    result += "<br>"
+    # create Sarsa strategy chart
+    result += "<table>"
 
-    for i in range(0, winningPoints*3-17):
-        result +=" "
-    result +="Sarsa"
-    result += "<br>"
+    # create header row
+    result += "<tr><td rowspan='12'>Dealer's Upcard</td><td colspan='12'>player (usable ace)</td><td></td><td colspan='12'>player (no usable ace)</td></tr>"
 
-    result +="              player (usable ace)"
-    for i in range(0, winningPoints*3-36):
-        result +=" "
-    result +="player (no usable ace)<br>"
 
-    result +="         "
+    # create top row of player cards
+    result += "<tr><td></td>"
     for p in range(11, winningPoints+1):
-        result += str(p)
-    result +="             "
+        result += "<td>{}</td>".format(p)
+    result += "<td></td>"
     for p in range(11, winningPoints+1):
-        result += str(p)
-    result += "<br>"
-
-    for d in range (1,11):
-        if (d == 10):
-            result +="      10   "
-        elif (d == 3):
-            result +="    D  3   "
-        elif (d == 4):
-            result +="    e  4   "
-        elif (d == 5):
-            result +="    a  5   "
-        elif (d == 6):
-            result +="    l  6   "
-        elif (d == 7):
-            result +="    e  7   "
-        elif (d == 8):
-            result +="    r  8   "
-        else:
-            result += "      " + str(d) + "  "
-
+        result += "<td>{}</td>".format(p)
+    result += "</tr>"
+    
+    # create rows for each dealer card
+    for d in range(1, 11):
+        # create row for dealer card
+        result += "<tr>"
+        # label dealer card
+        result += "<td>{}</td>".format(d if d != 1 else "A")
+        # create cells for player (usable ace)
         for p in range(11, winningPoints+1):
-            result +=HitStickOrDouble(QTableDictForSS[p,d,1,0], QTableDictForSS[p,d,1,1], QTableDictForSS[p,d,1,2])
-
-        if (d == 10):
-            result +="         10  "
-        elif (d == 3):
-            result +="      D   3  "
-        elif (d == 4):
-            result +="      e   4  "
-        elif (d == 5):
-            result +="      a   5  "
-        elif (d == 6):
-            result +="      l   6  "
-        elif (d == 7):
-            result +="      e   7  "
-        elif (d == 8):
-            result +="      r   8  "
-        else:
-            result += "      " + str(d) + "  "
-
+            cell_value = HitStickOrDouble(QTableDictForSS[p,d,1,0], QTableDictForSS[p,d,1,1], QTableDictForSS[p,d,1,2])
+            result += "<td>{}</td>".format(cell_value)
+        # label dealer card
+        result += "<td>{}</td>".format(d if d != 1 else "A")
+        # create cells for player (no usable ace)
         for p in range(11, winningPoints+1):
-            result +=HitStickOrDouble(QTableDictForSS[p,d,0,0], QTableDictForSS[p,d,0,1], QTableDictForSS[p,d,0,2])
+            cell_value = HitStickOrDouble(QTableDictForSS[p,d,0,0], QTableDictForSS[p,d,0,1], QTableDictForSS[p,d,0,2])
+            result += "<td>{}</td>".format(cell_value)
+        # close row
 
-        result += "<br>"
-    result += "<br>"
+        result += "</tr>"    
+    # close table
+    result += "</table>"    
+    
+    # add header and statistics for Temporal Difference
+    result += "<h3>Temporal Difference</h3>"
+    
+    # create Temporal Difference strategy chart
+    result += "<table>"    
+    # create header row
+    result += "<tr><td rowspan='12'>Dealer's Upcard</td><td colspan='12'>player (usable ace)</td><td></td><td colspan='12'>player (no usable ace)</td></tr>"
 
-    for i in range(0, winningPoints*3-23):
-        result +=" "
-    result +="Temporal Difference"
-    result += "<br>"
+# create top row of player cards
 
-    result +="              player (usable ace)"
-    for i in range(0, winningPoints*3-36):
-        result +=" "
-    result +="player (no usable ace)<br>"
-
-    result +="         "
+    # create top row of player cards
+    result += "<tr><td></td>"
     for p in range(11, winningPoints+1):
-        result += str(p)
-    result +="             "
+        result += "<td>{}</td>".format(p)
+    result += "<td></td>"
     for p in range(11, winningPoints+1):
-        result += str(p)
-    result += "<br>"
-
-    for d in range (1,11):
-        if (d == 10):
-            result +="      10   "
-        elif (d == 3):
-            result +="    D  3   "
-        elif (d == 4):
-            result +="    e  4   "
-        elif (d == 5):
-            result +="    a  5   "
-        elif (d == 6):
-            result +="    l  6   "
-        elif (d == 7):
-            result +="    e  7   "
-        elif (d == 8):
-            result +="    r  8   "
-        else:
-            result += "      " + str(d) + "  "
-
+        result += "<td>{}</td>".format(p)
+    result += "</tr>"
+    
+    # create rows for each dealer card
+    for d in range(1, 11):
+        # create row for dealer card
+        result += "<tr>"
+        # label dealer card
+        result += "<td>{}</td>".format(d if d != 1 else "A")
+        # create cells for player (usable ace)
         for p in range(11, winningPoints+1):
-             result +=HitStickOrDouble(QTableDictForMC[p,d,1,0], QTableDictForMC[p,d,1,1], QTableDictForMC[p,d,1,2])
-
-        if (d == 10):
-            result +="         10  "
-        elif (d == 3):
-            result +="      D   3  "
-        elif (d == 4):
-            result +="      e   4  "
-        elif (d == 5):
-            result +="      a   5  "
-        elif (d == 6):
-            result +="      l   6  "
-        elif (d == 7):
-            result +="      e   7  "
-        elif (d == 8):
-            result +="      r   8  "
-        else:
-            result += "      " + str(d) + "  "
-
+            cell_value = HitStickOrDouble(QTableDictForMC[p,d,1,0], QTableDictForMC[p,d,1,1], QTableDictForMC[p,d,1,2])
+            result += "<td>{}</td>".format(cell_value)
+        # label dealer card
+        result += "<td>{}</td>".format(d if d != 1 else "A")
+        # create cells for player (no usable ace)
         for p in range(11, winningPoints+1):
-            result +=HitStickOrDouble(QTableDictForMC[p,d,0,0], QTableDictForMC[p,d,0,1], QTableDictForMC[p,d,0,2])
-
-        result += "<br>"
-
+            cell_value = HitStickOrDouble(QTableDictForMC[p,d,0,0], QTableDictForMC[p,d,0,1], QTableDictForMC[p,d,0,2])
+            result += "<td>{}</td>".format(cell_value)
+        # close row
+        result += "</tr>"    
+    # close table
+    result += "</table>"    
+    
+    
+    
+    
     return render_template('result.html', result=result)
+
+
 
 
 
